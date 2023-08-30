f9162df975f24d366b08895ad988cd5a62b392bc
Shaun Chow <105621363+shaunchowGO@users.noreply.github.com>
2023-08-30 18:23:07 +1000

    API folder

---
+++ the-winning-team/src/utils/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const API_BASE_URL = '';
+
+export const getStudentProfile = (studentID) => = {
+    return axios.get(`${API_BASE_URL}/students/${studentID}`);
+
+}
+export const getTeacherProfile = (teacherID) => = {
+    return axios.get(`${API_BASE_URL}/teachers/${teacherID}`);
+
+}
