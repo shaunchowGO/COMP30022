ea7bbb3e7cd65f75d98cca0a7c4e1e27fb33da98
John Kavadias <123132297+Jkavadias21@users.noreply.github.com>
2023-09-08 12:30:10 +1000

    prototype for landing page

--- root/frontend/the-winning-team/src/pages/Landing.js
+++ root/frontend/the-winning-team/src/pages/Landing.js
@@ -3,16 +3,66 @@ import '../css/Landing.css'
 function Landing() {
   return (
     <section id="landing">
+      <div className='scrollable'>
         <div className="landing-main">
-            <div className="landing-main-left"> 
-                <h1>Lorem ipsum dolor sit</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                <button>Get Started</button>
-            </div>
-            <div className="landing-main-right">
+          <div className="landing-main-left">
                 <img src="" alt="landing-img"></img>
-            </div>
+          </div>
+          <div className="landing-main-right"> 
+            <h1>Lorem ipsum dolor sit</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <button>Get Started</button>
+          </div>
+        
         </div>
+        <div className = "instructions1">
+          <div className = "instruction-left"> 
+            <img src="" alt="instruction-image"></img>
+          </div>
+
+          <div className = "right-text">
+            <h1>Lorem ipsum dolor sit</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          </div>
+        </div>
+        
+        <div className = "instructions2">
+           <div className = "right-text">
+            <h1>Lorem ipsum dolor sit</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          </div>
+          
+          <div className = "instruction-left"> 
+            <img src="" alt="instruction-image"></img>
+          </div>
+        </div>
+        
+        <div className = "FAQ-description"> 
+          <h1> FAQs</h1>
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        </div>
+
+        <div className = "FAQs"> 
+          <div className = "FAQ1"> 
+            <h1>FAQ 1</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            </div>
+            
+            <div className = "FAQ2">
+              <h1>FAQ 2</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            </div>
+            
+            <div className = "FAQ3"> 
+              <h1>FAQ 3</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            </div>
+
+        </div>
+      </div>
+       
+        
+        
     </section>
   );
 }
