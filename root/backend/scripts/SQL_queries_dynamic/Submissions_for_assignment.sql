c5b709c23c5691916dfbc23441c39fb6f7be9b2b
younis1 <younis.mamoun2001@gmail.com>
2023-10-07 19:02:16 +1100

    Update Submissions_for_assignment.sql

--- root/backend/scripts/SQL_queries_dynamic/Submissions_for_assignment.sql
+++ root/backend/scripts/SQL_queries_dynamic/Submissions_for_assignment.sql
@@ -1,4 +1,4 @@
 select std.Name, sub.similarity_score, sub.[date] as [Date Added] from [dbo].Student as std
-inner join [dbo].[Submissions] as sub
+inner join [dbo].[Submission] as sub
 on std.Id = sub.StudentId
 where sub.AssignmentId = REPLACE_WITH_ASSIGNMENT_ID
