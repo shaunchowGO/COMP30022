7537bc5e54b06b30d8f7d1600b91036891144fad
younis1 <younis.mamoun2001@gmail.com>
2023-10-07 19:44:21 +1100

    Uploading missing files

--- root/backend/scripts/SQL_queries_dynamic/Submissions_for_studentsql.sql
+++ root/backend/scripts/SQL_queries_dynamic/Submissions_for_studentsql.sql
@@ -1,2 +1,6 @@
-select * from [dbo].Submission
-where StudentID = REPLACE_WITH_STUDENT_ID
+select sub.AssignmentId, s.Name as [Subject Name], sub.Similarity_Score, [Date]  from [dbo].Submission as sub
+inner join [dbo].[Assignment] as  A
+on sub.AssignmentId = A.Id
+inner join [dbo].[Subject] as s
+on s.Id = A.subjectId
+where sub.studentId = REPLACE_WITH_STUDENT_ID
